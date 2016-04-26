9cfe73db84e68ad6e02565d60e0a6aec41d90ee7
Tony Kung <lps.tony@gmail.com>
2016-04-26 11:31:30 -0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,30 @@
+ $(document).ready(function(){
+
+          $("time.timeago").timeago();
+
+          var index = streams.home.length - 1; //the last item of home array; 
+
+          while(index >= 0){    
+            var tweet = streams.home[index];
+            var $tweet = $('<div></div>');
+            var $name = $('<span class="name"></span>'); 
+            var name = tweet.user; 
+            var clicked = jQuery.timeago(new Date()); 
+            $tweet.text(tweet.message); //adds text to $tweet. //'@' + ': ' + 
+            $name.text('@' + name + ': ');
+            $name.prependTo($tweet);  
+            $tweet.appendTo($('.container'));  //adds $tweet to $body. 
+            $tweet.append('<span class="time">' + clicked + '</span>');
+            index -= 1;
+          }
+
+
+
+        $('name').on('click', function(){
+            addTweet();
+            $name.prependTo($tweet);  
+            $tweet.appendTo($('.container'));  //adds $tweet to $body. 
+            $tweet.append('<span class="time">' + clicked + '</span>');
+        });
+        
+});
