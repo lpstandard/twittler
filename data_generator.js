62f3c43e8f6243939b8e763a5134e27e0e330975
Tony Kung <lps.tony@gmail.com>
2016-04-27 12:45:37 -0500

    add use timeline

--- data_generator.js
+++ data_generator.js
@@ -15,7 +15,7 @@ window.users = Object.keys(streams.users);
 
 // utility function for adding tweets to our data structures
 var addTweet = function(newTweet){
-  var username = newTweet.user;
+  var username = newTweet.user;  
   streams.users[username].push(newTweet);
   streams.home.push(newTweet);
 };
--- script.js
+++ script.js
@@ -1,33 +1,28 @@
- $(document).ready(function(){
+$(document).ready(function(){
 
-          $('button').on('click', function(){
+  $('button').on('click', function(){
 
+    $("time.timeago").timeago();
 
-            $("time.timeago").timeago();
+    var index = streams.home.length - 1; //initial index is 9 for home array; 
+      while(index >= 0){      //as long as there tweets in the streams.home array, keep generating tweets.
+        var tweet = streams.home[index];
+        var $tweet = $('<div class="tweet '+ tweet.user +'"></div>');
+        var $name = $('<span class="name"></span>'); 
+        var names = tweet.user; 
+        var timeStamp = jQuery.timeago(new Date()); 
+        $tweet.text(tweet.message); //adds text to $tweet. //'@' + ': ' + 
+        $name.text('@' + names + ':');
+        $name.prependTo($tweet);  
+        $tweet.appendTo($('.container'));  //adds $tweet to $body. 
+        $tweet.append('<span class="time">' + timeStamp + '</span>');
+        index -= 1;
+      }
+      $('.name').on('click', function(){
+        var userName = $(this).text();
+        userName = userName.slice(1, userName.length - 1);
+        $(".tweet:not(."+userName+')').remove();
 
-            var index = streams.home.length - 1; //the last item of home array; 
-
-            while(index >= 0){    
-              var tweet = streams.home[index];
-              var $tweet = $('<div></div>');
-              var $name = $('<span class="name"></span>'); 
-              var name = tweet.user; 
-              var clicked = jQuery.timeago(new Date()); 
-              $tweet.text(tweet.message); //adds text to $tweet. //'@' + ': ' + 
-              $name.text('@' + name + ': ');
-              $name.prependTo($tweet);  
-              $tweet.appendTo($('.container'));  //adds $tweet to $body. 
-              $tweet.append('<span class="time">' + clicked + '</span>');
-              index -= 1;
-            }
-          });
-
-
-          $('name').on('click', function(){
-              addTweet();
-              $name.prependTo($tweet);  
-              $tweet.appendTo($('.container'));  //adds $tweet to $body. 
-              $tweet.append('<span class="time">' + clicked + '</span>');
-          });
-        
+      });             
+  });
 });
